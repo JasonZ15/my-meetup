38a32cfa414c558c7bd3b9cfd8aa05187bdb409f
JasonZ15 <vines15@gmail.com>
2014-12-08 07:53:39 -0800

    adding bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -350,6 +350,11 @@ module.exports = function (grunt) {
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
+        }, {
+          expand: true,
+          cwd: 'bower_components/bootstrap/dist',
+          src: 'fonts/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
